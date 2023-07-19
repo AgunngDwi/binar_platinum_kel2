40312ce2b681f8c999a30545be96770f41beab9a
korundum1001 <madanebook@gmail.com>
2023-07-19 10:02:57 +0800

    make template for admin dashboard and make path route to that page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Findcar from "./pages/Homepage/Findcar"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Detailcar from "./pages/Homepage/Detailcar";
 import AdminLogin from "./components/Admin/Login";
+import AdminDashboard from "./components/Admin/Dashboard";
 // import './css/master.css'
 
 
@@ -24,6 +25,7 @@ function App() {
           <Route path="cars/*" element={<Findcar />} />
           <Route path="/detail/:id" element={<Detailcar />} />
           <Route path="/admin" element={<AdminLogin/>}/>
+          <Route path="/admin_dashboard" element={<AdminDashboard/>} />
         </Routes>
       </BrowserRouter>
     </div>
