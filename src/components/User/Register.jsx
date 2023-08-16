825c631adf363fc6108f8fff9ca0b85e9c6552b2
Agung Dwi Laksono <agungdwilaksono60@gmail.com>
2023-08-16 21:04:19 +0700

    fix register css

--- src/components/User/Register.jsx
+++ src/components/User/Register.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useState } from "react";
 import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import "./register.css";
+import "./Register.css";
 
 const Register = () => {
   const [form, setForm] = useState({
